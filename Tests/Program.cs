88fd4fda6e8a8e55fff372c5909013d2f1ced581
Emin Hasanov <developerPazl@gmail.com>
2026-02-07 16:39:14 +0400

    Test changed

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,13 +1,4 @@
 ﻿
-Console.WriteLine("Hello, World!");
-
-//using DataAccess.EfImplementations;
-//using DataAccess.EfImplementations.Contexts;
-//using DataAccess.UnitOfWorks;
-
-//IUnitOfWork unitOfWork = new EfUnitOfWork(new DersDb2Context());
-
-//var person = unitOfWork.PersonRepository.FindByEmail("davud@gmail.com");
 //if (person == null)
 //{
 //    Console.WriteLine("Musteri tapilmadi");
