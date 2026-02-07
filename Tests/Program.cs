2af195a1c55829f9bbd6edf16fafa727c14ce9df
Emin Hasanov <developerpazl@gmail.com>
2026-02-07 16:34:43 +0400

    Clean up Program.cs by removing unused code
    Removed commented-out code and retained the Hello World message.

--- Tests/Program.cs
+++ Tests/Program.cs
@@ -1,18 +1,2 @@
 ﻿
 Console.WriteLine("Hello, World!");
-
-//using DataAccess.EfImplementations;
-//using DataAccess.EfImplementations.Contexts;
-//using DataAccess.UnitOfWorks;
-
-//IUnitOfWork unitOfWork = new EfUnitOfWork(new DersDb2Context());
-
-//var person = unitOfWork.PersonRepository.FindByEmail("davud@gmail.com");
-//if (person == null)
-//{
-//    Console.WriteLine("Musteri tapilmadi");
-//}
-//else
-//{
-//    Console.WriteLine($"{person.Name} {person.Surname}");
-//}
